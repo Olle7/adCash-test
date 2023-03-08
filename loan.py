1267b6030646350c3aa99080cbd17b9e61059382
Olger Männik <Olgerm@protonmail.com>
2023-03-08 19:51:48 +0200

    all responses to form are now on same URL.

--- loan.py
+++ loan.py
@@ -4,34 +4,27 @@ from sqlite3 import connect
 app = Flask(__name__)
 
 def denie_beacuase_of_too_many_applications(personal_ID):
-    return True
+    return False
 def denie_beacuase_blacklisted(personal_ID):
     conn = connect('loan_applications.db')
     cursor = conn.cursor()
-
     cursor.execute("SELECT COUNT(*) FROM blacklisted_personal_IDs WHERE personal_id=?", (personal_ID,))
     count = cursor.fetchone()[0]
-
     conn.close()
-
     return count > 0
 
 @app.route("/",methods=["GET","POST"])#this is the mainpage
 def main_page():
     return render_template("main_page.html")
 
-@app.route("/application_denied_because_too_many_applications",methods=["GET","POST"])#this is the mainpage
-def application_denied_because_too_many_applications_page():
-    return render_template("application deinied because too many applications.html")
-
 @app.route("/response_to_application",methods=["GET","POST"])
 def response_to_application():
     args_of_request={**dict(request.args),**dict(request.form)}
     add_loan_application_to_database(args_of_request["amount"],args_of_request["currency"], args_of_request["term"],args_of_request["name"], args_of_request["personal_ID"],args_of_request["contact"],args_of_request["type_of_contact"],args_of_request["comment"])
     if denie_beacuase_of_too_many_applications(args_of_request["personal_ID"]):
-        return redirect("/application_denied_because_too_many_applications")
+        return render_template("application deinied because too many applications.html")
     if denie_beacuase_blacklisted(args_of_request["personal_ID"]):
-        return redirect("/application_denied_because_blacklisted")
+        return render_template("application denied because blacklisted.html")
     return render_template("Application recieved.html")
 
 @app.route("/apply_for_a_loan",methods=["GET","POST"])
