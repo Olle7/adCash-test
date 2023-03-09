c2069d3c4de6e7a49eb9d57daedd706813b51565
Olger Männik <Olgerm@protonmail.com>
2023-03-10 01:58:58 +0200

    changed URLs and order of function definitions in sorcecode.

--- loan.py
+++ loan.py
@@ -22,9 +22,8 @@ def denie_beacuase_of_too_many_applications(personal_ID):
     rows = cursor.fetchall()
     count = len(rows)
     conn.close()
-    print("number of applications in last 60 seconds:", count)
+    #print("number of applications in last 60 seconds:", count)
     return count > MAX_APPLICATIONS_IN_24_H
-
 
 def denie_beacuase_blacklisted(personal_ID):
     conn = connect('loan_applications.db')
@@ -33,25 +32,6 @@ def denie_beacuase_blacklisted(personal_ID):
     count = cursor.fetchone()[0]
     conn.close()
     return count > 0
-
-@app.route("/",methods=["GET","POST"])#this is the mainpage
-def main_page():
-    return render_template("main_page.html")
-
-@app.route("/response_to_application",methods=["GET","POST"])
-def response_to_application():
-    args_of_request=get_all_arguments(request)
-    #print("args:",args_of_request)
-    if denie_beacuase_of_too_many_applications(args_of_request["personal_ID"]):
-        return render_template("application deinied because too many applications.html")
-    if denie_beacuase_blacklisted(args_of_request["personal_ID"]):
-        return render_template("application denied because blacklisted.html")
-    add_loan_application_to_database(args_of_request["amount"],args_of_request["currency"], args_of_request["term"],args_of_request["name"], args_of_request["personal_ID"],args_of_request["contact"],args_of_request["type_of_contact"],args_of_request["comment"])
-    return render_template("Application recieved.html")
-
-@app.route("/apply_for_a_loan",methods=["GET","POST"])
-def apply_for_a_loan_page():
-    return render_template("apply_for_a_loan_form.html")
 
 def add_loan_application_to_database(amount,currency,term,name,personal_id,contact,type_of_contact,comment):
     # Connect to the SQLite database
@@ -68,6 +48,46 @@ def add_loan_application_to_database(amount,currency,term,name,personal_id,conta
     # Commit the changes and close the connection
     conn.commit()
     conn.close()
+
+
+
+
+@app.route("/",methods=["GET","POST"])#this is the mainpage
+def main_page():
+    return render_template("main_page.html")
+
+
+
+@app.route("/response_to_application",methods=["GET","POST"])
+def response_to_application():
+    args_of_request=get_all_arguments(request)
+    if denie_beacuase_of_too_many_applications(args_of_request["personal_ID"]):
+        return render_template("application deinied because too many applications.html")
+    if denie_beacuase_blacklisted(args_of_request["personal_ID"]):
+        return render_template("application denied because blacklisted.html")
+    add_loan_application_to_database(args_of_request["amount"],args_of_request["currency"], args_of_request["term"],args_of_request["name"], args_of_request["personal_ID"],args_of_request["contact"],args_of_request["type_of_contact"],args_of_request["comment"])
+    return render_template("Application recieved.html")
+
+@app.route("/apply_for_a_loan_form",methods=["GET","POST"])
+def apply_for_a_loan_page():
+    return render_template("apply_for_a_loan_form.html")
+
+
+
+@app.route("/table_of_loans_by_borrower",methods=["GET","POST"])
+def table_of_loans_by_borrower():
+    args_of_request=get_all_arguments(request)
+    if "borrower" in args_of_request:
+        pass
+    else:
+        pass
+    return render_template("table_of_floans_by_borrower.html")
+
+@app.route("/list_of_loans_by_borrower",methods=["GET","POST"])
+def list_of_loans_by_borrower():
+    args_of_request=get_all_arguments(request)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
