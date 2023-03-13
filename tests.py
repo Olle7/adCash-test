bd6c783cb43bad0ed41d51df2976989fcfdef5b7
Olger Männik <Olgerm@protonmail.com>
2023-03-13 02:32:06 +0200

    added tests

---
+++ tests.py
@@ -0,0 +1,16 @@
+import requests
+
+# The API endpoint
+url="http://127.0.0.1:5000/response_to_application"
+
+# POST
+assert requests.post(url,{"amount":"100","currency":"100","term":"100","name":"100","personal_ID":"100","contact":"100","type_of_contact":"100","comment":"100"}).status_code==200
+
+#GET
+assert requests.get(url+"?amount=&contact=GETT&currency=CUR&comment=YYY&term=ttt&name=olger&personal_ID=123&type_of_contact=gsm").status_code==200
+
+#GET+POST overlapping params
+assert requests.post(url+"?amount=111&contact=Gmail&currency=GCUR&comment=YYY&term=ttt&name=olger&personal_ID=123&type_of_contact=gsm",{"amount":"982","currency":"100","term":"100","name":"100","personal_ID":"100","contact":"100","type_of_contact":"100","comment":"100"}).status_code!=200
+
+#GET+POST not overlapping params
+assert requests.post(url+"?amount=111&contact=Gmail&currency=GCUR",{"term":"100","name":"100","personal_ID":"100","type_of_contact":"100","comment":"100"}).status_code==200
